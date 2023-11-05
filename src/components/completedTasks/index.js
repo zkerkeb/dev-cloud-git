db12ff05d9a9c18e6f4e4ae76dd968c28d6030a8
Zak <hikkary@MacBookAirde.local>
2023-11-05 20:01:55 +0100

    feat(prettier/linter):done

--- src/components/completedTasks/index.js
+++ src/components/completedTasks/index.js
@@ -1,23 +1,21 @@
-import React, { useContext } from 'react';
-import { TasksContext } from '../../utils/contexts/taskContext';
+import React, {useContext} from 'react';
+import {TasksContext} from '../../utils/contexts/taskContext';
 
 const CompletedTasks = () => {
-    // On recupere les task dans le context pour les afficher
-    const { tasks } = useContext(TasksContext);
-    const completedTasks = tasks.filter(task => task.completed);
+  // On recupere les task dans le context pour les afficher
+  const {tasks} = useContext(TasksContext);
+  const completedTasks = tasks.filter(task => task.completed);
 
-    return (
-        <div>
-            <h2>Tâches terminées</h2>
-            <ul>
-                {completedTasks.map(task => (
-                    <li key={task.id}>
-                        {task.text}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Tâches terminées</h2>
+      <ul>
+        {completedTasks.map(task => (
+          <li key={task.id}>{task.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default CompletedTasks;
