103f323c8928d146fa5c852fcbdee70434fbc85e
Zakaria Kerkeb <hikkary@mac.local>
2023-11-07 14:16:05 +0100

    test husky

--- src/utils/contexts/taskContext.js
+++ src/utils/contexts/taskContext.js
@@ -1,6 +1,7 @@
-import { createContext } from 'react';
+import {createContext} from 'react';
 // Cree le contexte, il est ici uniquemement cree, il n'est pas encore utilise
 // Il sera utilise dans src/components/todoList/index.js
 // C'est pour faire propre
-export const TasksContext = createContext() 
- 
+export const TasksContext = createContext();
+
+const wesh = 'wesh';
